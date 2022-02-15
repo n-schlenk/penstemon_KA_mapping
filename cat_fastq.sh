f1327105f007d37f6b3c9895bb910d32deaf366c
n-schlenk <99570595+n-schlenk@users.noreply.github.com>
2022-02-15 15:09:41 -0600

    Update cat_fastq.sh

--- cat_fastq.sh
+++ cat_fastq.sh
@@ -6,18 +6,18 @@
 
 #!/bin/bash
 mkdir cat_edits                                                             # make directory for concatenated fastq files
-A=$(awk '{print $1}' $1 | uniq )                                            # assemble file with one ID per line, no duplicates
+A=$(awk '{print $1}' $1 | uniq )                                            # define variable A as one ID per line, no duplicates
 N=$(awk '{print $1}' $1 | uniq | sed '/^$/d' | wc -l)                       # calculate number of unique IDs
 for i in $(seq 1 $N)                                                        # start loop from 1 to last unique ID in list
 do
-    B=$(echo $A | cut -d ' ' -f $i)                                         # define ID within loop
+    B=$(echo $A | cut -d ' ' -f $i)                                         # define variable B as the ID within loop
     C=$(grep $B $1 | awk '{print $3}')                                      # extract location(s) for ID
     D=$(grep -c $B $1)                                                      # count number of duplicates
     if [[ $D == 1 ]]                                                        # if there are no duplicates
     then                                                                    # then copy file to new directory
         cp ./$C'_edits/'$B'.trimmed_R1_.fastq.gz' ./cat_edits
     elif [[ $D == 2 ]]                                                      # if there are 2 duplicates
-    then                                                                    # then cat the, and move to new directory
+    then                                                                    # then cat them and move to new directory
         E=$(echo $C | cut -d ' ' -f 1)
         F=$(echo $C | cut -d ' ' -f 2)
         cat ./$E'_edits/'$B'.trimmed_R1_.fastq.gz' ./$F'_edits/'$B'.trimmed_R1_.fastq.gz' > ./cat_edits/$B'_cat_edts_trimmed_R1_.fastq.gz'
