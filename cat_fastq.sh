8edeb10b3dc14308beddbce5603cbb2fbbd203b2
n-schlenk <99570595+n-schlenk@users.noreply.github.com>
2022-02-14 18:07:57 -0600

    cat_fastq.sh
    sh cat_fastq.sh [INPUT]
    INPUT: ID, read count, location
    OUTPUT: directory (cat_edits) with one fastq file per individual

---
+++ cat_fastq.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+mkdir cat_edits                                                             # make directory for concatenated fastq files
+A=$(awk '{print $1}' $1 | uniq )                                            # assemble file with one ID per line, no duplicates
+for i in {1..6}
+do
+    B=$(echo $A | cut -d ' ' -f $i)                                         # define ID within loop
+    C=$(grep $B $1 | awk '{print $3}')                                      # extract location(s) for ID
+    D=$(grep -c $B $1)                                                      # count number of duplicates
+    if [[ $D == 1 ]]                                                        # if there are no duplicates
+    then                                                                    # then copy file to new directory
+        cp ./$C'_edits/'$B'.trimmed_R1_.fastq.gz' ./cat_edits
+    elif [[ $D == 2 ]]                                                      # if there are 2 duplicates
+    then                                                                    # then cat the, and move to new directory
+        E=$(echo $C | cut -d ' ' -f 1)
+        F=$(echo $C | cut -d ' ' -f 2)
+        cat ./$E'_edits/'$B'.trimmed_R1_.fastq.gz' ./$F'_edits/'$B'.trimmed_R1_.fastq.gz' > ./cat_edits/$B'_cat_edts_trimmed_R1_.fastq.gz'
+    elif [[ $D == 3 ]]                                                      # if there are 3 duplicates
+    then                                                                    # then cat them and move to new directory
+        E=$(echo $C | cut -d ' ' -f 1)
+        F=$(echo $C | cut -d ' ' -f 2)
+        G=$(echo $C | cut -d ' ' -f 3)
+        cat ./$E'_edits/'$B'.trimmed_R1_.fastq.gz' ./$F'_edits/'$B'.trimmed_R1_.fastq.qz' ./$G'_edits/'$B'.trimmed_R1_.fastq.gz' > ./cat_edits/$B'_cat_edits_trimmed_R1_.fastq.gz'
+    else
+        echo $B has more than 3 duplicates                                  # this could go on forever
+    fi
+done
