3f2563809e4b7c7cda5a138311849daed607b0fd
n-schlenk <99570595+n-schlenk@users.noreply.github.com>
2022-04-22 11:01:48 -0500

    Delete cat_fastqs.sh

--- cat_fastqs.sh
+++
@@ -1,9 +0,0 @@
-# this script takes every fastq edit file for every unique ID and combines, each ID in one cat file
-# ID.txt contains each ID once
-
-#!/bin/bash
-for i in $(cat ID.txt)
-do
-    A=$(find -name "$i.*.gz")
-    cat $A >> $i'.cat.fastq.gz'
-done
