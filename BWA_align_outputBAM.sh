c8e9aa3d0f540ce98f2416b6848fbbd59fedca85
n-schlenk <99570595+n-schlenk@users.noreply.github.com>
2022-06-06 12:35:48 -0500

    align fastqs to ref genome, output edited BAM

--- BWA_align_outputBAM.sh
+++ BWA_align_outputBAM.sh
@@ -1,16 +1,9 @@
+# index reference genome, align each fastq to reference genome (results in SAM file)
+# AddOrReplaceReadGroups, translate to BAM, sort, optional MarkDuplicates
+# index resulting BAM file
+# created by N. Schlenk
+
 #!/bin/bash
-#SBATCH --job-name=bwa_stuff    # Job name
-#SBATCH --partition=eeb           # Partition Name (Required)
-#SBATCH --mail-type=BEGIN,END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
-#SBATCH --mail-user=n752s925@ku.edu      # Where to send mail
-#SBATCH --ntasks=1                    # Run one task
-#SBATCH --cpus-per-task=8             # CPU cores per task
-#SBATCH --mem=20gb                     # Job memory request
-#SBATCH --time=2-24:00:00             # Time limit days-hrs:min:sec
-#SBATCH --output=serial_test_%j.log   # Standard output and error log
-
-cd $SLURM_SUBMIT_DIR
-
 module load bwa
 module load java
 module load picard
