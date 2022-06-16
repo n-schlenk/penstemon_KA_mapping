ae43c0822dff1510c5718888d9fc67762fdf1cae
n-schlenk <99570595+n-schlenk@users.noreply.github.com>
2022-06-16 10:36:34 -0500

    generate coordinates for final map

--- coords_for_final_plot.py
+++ coords_for_final_plot.py
@@ -1,28 +1,66 @@
 #! /usr/bin/env python3
 # written by N. Schlenk
-# creates file with coordinates for plotting Lep-MAP3 output
+# returns file with marker number, scaffold, position, and linkage group
 
 import re
 import os
 
-vcf=open('./LG8/LG_8.vcf', 'r')                                         # vcf of scaffold
-map=open('./LG8/ordered_lod3_LG8.txt', 'r')                             # ordered map file of scaffold
-out=open('./LG8/coords_LG8.txt', 'a')                                   # output file
+vcf=open('[filtered VCF]', 'r')
+map=open('[ordered map TXT]', 'r')  # same as map2
+map2=open('[ordered map TXT]', 'r') # same as map
+outfile=open('[coords TXT]', 'w')
 
-enum=1
-bp={}
-for line in vcf:                                                        # populate bp dictionary, keys = marker num, vals = bp
-    if line[0] != '#':                                                  
-        line_splat = line.split('\t')
-        bp[str(enum)] = str(line_splat[1])                             
-        enum += 1
-
+LG_start={}
+LG_end={}
+pos={}
+scaff={}
 cm={}
-for line in map:                                                        # populate cm dictionary, keys = marker num, vals = cm
+lg={}
+outline = ''
+
+
+def split_map(map_file):
+    header_lines = {}
+    for n,line in enumerate(map_file):
+        line_splat = line.split()
+        if line_splat[0] == '#***':
+            header_lines[int(line_splat[3])] = n
+        last_line = n
+    for i in range(1, len(header_lines.keys()) + 1):
+        start_pos = header_lines[i] + 2
+        if i < len(header_lines.keys()):
+            end_pos = header_lines[i + 1] - 1
+        elif i == len(header_lines.keys()):
+            end_pos = n
+        LG_start[i] = start_pos
+        LG_end[i] = end_pos
+    return(LG_start)
+    return(LG_end)
+
+split_map(map)
+
+n = 1
+for line in vcf:		
     if line[0] != '#':
         line_splat = line.split('\t')
-        cm[str(line_splat[0])] = str(line_splat[1])
+        if line_splat[0] != 'CHR':
+            pos[n] = line_splat[1]         		        	# populate pos dict as: keys = line (marker) number, values = position in bp
+            scaff[n] = line_splat[0]	    			    # populate scaff dict as: keys = line (marker) number, values = scaffold
+            n += 1
 
-for marker_num in cm.keys():                                            # write output to file, base number followed by centimorgan value
-    output = str(bp[marker_num]+'\t'+cm[marker_num]+'\n')
-    out.write(output)
+n = 0
+for line in map2:
+    if line[0] != '#':
+        line_splat = line.split('\t')
+        cm[int(line_splat[0])] = line_splat[1]  	    	# populate cm dict as: keys = marker number, values = position in cM
+    for i in LG_start.keys():
+        if n >= LG_start[i] and n <= LG_end[i]:
+            lg[int(line_splat[0])] = i	            	    # populate lg dict as: keys = marker number, values = LG assigned in Lep-MAP3    
+    n += 1
+
+for marker_num in cm.keys():
+    # outline = str(lg[marker_num]) + '\t' + scaff[marker_num] + '\t' + pos[marker_num] + '\t' + cm[marker_num] + '\n'
+    outline = str(lg[marker_num]) + '\n'
+    outfile.write(outline)
+
+outfile.close()
