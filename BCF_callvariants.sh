2e582be3864ff60b5a579118d0c1465f4333897d
n-schlenk <99570595+n-schlenk@users.noreply.github.com>
2022-06-23 12:26:07 -0500

    call variants

--- BCF_callvariants.sh
+++ BCF_callvariants.sh
@@ -1,8 +1,7 @@
-# call variants on BAM files, output one VCF
+#!/bin/bash
+# call variants on BAM files (must be in 'bam_files' directory), output one VCF
 # created by N. Schlenk
 
-#!/bin/bash
-
 module load bcftools
-ls ./SRGMD > bam_list.txt
+ls ./bam_files > bam_list.txt
 bcftools mpileup -Ou -I -a FORMAT/AD --max-depth=100 -f PGA_assembly_trimmed.fasta -b bam_list.txt | bcftools call -vmO v > called.vcf
