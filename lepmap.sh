1ddc4b9d1839cbdf6b90d9fcdfe70657c6c1abf9
n-schlenk <99570595+n-schlenk@users.noreply.github.com>
2022-06-25 10:50:13 -0500

    lepmap pipeline for linkage mapping

--- lepmap.sh
+++ lepmap.sh
@@ -1,8 +1,7 @@
 #! bin/bash
 
-java -cp /panfs/pfs.local/work/hileman/n752s925/bin ParentCall2 data=pedigree.txt vcfFile=KA_called.vcf >> parentcalled.vcf
-java -cp /panfs/pfs.local/work/hileman/n752s925/bin Filtering2 data=parentcalled.vcf removeNonInformative=1 dataTolerance=0.0000001 >> filtered.vcf
-java -cp /panfs/pfs.local/work/hileman/n752s925/bin SeparateChromosomes2 data=filtered.vcf lodLimit=5 > map.txt
-java -cp /panfs/pfs.local/work/hileman/n752s925/bin JoinSingles2All map=map.txt data=filtered.vcf lodLimit=4 > joined_map.txt
-java -cp /panfs/pfs.local/work/hileman/n752s925/bin OrderMarkers2 map=joined_map.txt data=filtered.vcf chromosome=PGA_scaffold0__171_contigs__length_68081433 > ordered1.txt
+java -cp [path] ParentCall2 data=pedigree.txt vcfFile=KA.vcf >> parentcalled.vcf
+java -cp [path] Filtering2 data=parentcalled.vcf removeNonInformative=1 dataTolerance=0.0000001 >> filtered.vcf
+# use separate_scaffold.sh, then run OrderMarkers2 on each scaffold individually
+java -cp [path] OrderMarkers2 evaluateOrder=[scaffold map] data=[scaffold VCF] sexAveraged=1 improveOrder=0 > [linkage map for one scaffold]
 
