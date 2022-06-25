e67b6ccfb6dcec38c688784bfd82744695b1d40b
n-schlenk <99570595+n-schlenk@users.noreply.github.com>
2022-06-25 10:39:21 -0500

    recombination count from log file

--- recomb_count.py
+++ recomb_count.py
@@ -1,23 +1,24 @@
 #! /usr/bin/env python3
 # Created by N. Schlenk
-# Extracts ID and recombination count from log file after looping through OrderMarkers2 4 times in each of the 8 linkage groups
+# Extracts ID and recombination count from log file after looping through OrderMarkers2 [loop] times in each linkage group
 
 import os
 import re
 
-recom_log=open('serial_test_35520326.log', 'r')
-outfile=open('order5_recombination_log.txt', 'w')
+recom_log=open('[log file]', 'r')
+outfile=open('[recombination log]', 'w')
 
 order = 0
 lg = 1
+loop = 4                                                            # number of times OrderMarkers2 was run
 start = []
 
 for n,line in enumerate(recom_log):
     outline = ''
     line_splat = line.split(' ')
-    if line_splat[0] == 'number' and order < 4:
+    if line_splat[0] == 'number' and order < loop:
         order += 1
-    elif order == 4:
+    elif order == loop:
         if line_splat[0].split('\t')[0] == 'Individual':
             outline = line_splat[0].split('\t')[2] + '\t' + str(line_splat[0].split('\t')[4]) + '\t' + str(lg) + '\n'
             outfile.write(outline)
