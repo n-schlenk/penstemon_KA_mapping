117abeab15f6b9ae6fe433443530247988a1ae73
n-schlenk <99570595+n-schlenk@users.noreply.github.com>
2023-02-10 17:41:47 -0800

    Update calc_genfreq.py

--- calc_genfreq.py
+++ calc_genfreq.py
@@ -7,18 +7,18 @@ import os
 from numpy import mean
 from numpy import std
 
-invcf = open('filteredvcf.vcf', 'r')
+invcf = open(<filepath to vcf>, 'r')
 
-tot_RR = []
-tot_RA = []
-tot_AA = []
+tot_RR = 0
+tot_RA = 0
+tot_AA = 0
 for n,line in enumerate(invcf):
-    RR = 0
-    RA = 0
-    AA = 0
-    if n > 37:
-	line_splat = line.split('\t')
-        for i in range(187):
+    if line[0] != '#' and line[0:3] != 'CHR' and line[0:3] == 'PGA':    # this is specific to our assembly, change to ignore columns beginning with name of assembly
+        RR = 0
+        RA = 0
+        AA = 0
+        line_splat = line.split('\t')
+        for i in range(len(line_splat) - 2):
             id = line_splat[i]
             geno = id.split(':')
             if geno[0] == '0/0':
@@ -27,10 +27,17 @@ for n,line in enumerate(invcf):
                 RA += 1
             elif geno[0] == '1/1':
                 AA += 1
-        tot_RR.append(RR)
-        tot_RA.append(RA)
-        tot_AA.append(AA)
-print(mean(tot_RR), std(tot_RR))
-print(mean(tot_RA), std(tot_RA))
-print(mean(tot_AA), std(tot_AA))
+        tot_RR += RR
+        tot_RA += RA
+        tot_AA += AA
+
+tot_snps = tot_RR + tot_RA + tot_AA
+tot_RR = round((100 * tot_RR / tot_snps), 3)
+tot_RA = round((100 * tot_RA / tot_snps), 3)
+tot_AA = round((100 * tot_AA / tot_snps), 3)
+print('TOTAL genotype frequencies:')
+print('AA : ' + str(tot_RR))
+print('AB : ' + str(tot_RA))
+print('BB : ' + str(tot_AA))
+
 
