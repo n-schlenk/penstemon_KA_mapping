49c1779efba7de8b103af2a5d47d455a1200d855
n-schlenk <99570595+n-schlenk@users.noreply.github.com>
2023-02-10 17:59:53 -0800

    Update plot_map.py

--- plot_map.py
+++ plot_map.py
@@ -1,67 +1,44 @@
 #! /usr/bin/env python3
 # written by N. Schlenk
 # input map coordinates, output plot of 8 scaffolds
+# you can plot another number of scaffolds but you will need to adjust the plot code
 
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 
-co0 = open('plot2/coords0_v2.txt', 'r')
-co1 = open('plot2/coords1_v2.txt', 'r')
-co2 = open('plot2/coords2_v2.txt', 'r')
-co3 = open('plot2/coords3_v2.txt', 'r')
-co4 = open('plot2/coords4_v2.txt', 'r')
-co5 = open('plot2/coords5_v2.txt', 'r')
-co6 = open('plot2/coords6_v2.txt', 'r')
-co7 = open('plot2/coords7_v2.txt', 'r')
-
-co = [co0, co1, co2, co3, co4, co5, co6, co7]
+first_scaffold = 0
+last_scaffold = 7
+filepath_prefix = 'coords'
+filepath_suffix = '.txt'
+graph_color = 'darkblue'
+titles = ['LG0', 'LG1', 'LG2', 'LG3', 'LG4', 'LG5', 'LG6', 'LG7']
 
 dict = {}
-for i in range(8):
-    dict.setdefault(i, ([], []))
-    for n, scaffold in enumerate(co):
-        if n == i:
-            for line2 in scaffold:
-                dict[i][0].append(int(line2.split()[0]))
-                dict[i][1].append(float(line2.split()[1]))
-
-
-
-
+for i in range(first_scaffold, last_scaffold + 1):
+    dict.setdefault(i, ([],[]))
+    with open(filepath_prefix + str(i) + filepath_suffix, 'r') as infile:
+        for line in infile:
+            dict[i][0].append(int(line.split()[0]))
+            dict[i][1].append(float(line.split()[1]))
 
 fig, ax = plt.subplots(4, 2, sharex = 'col', sharey = 'row', constrained_layout = True)
-ax[0, 0].scatter(dict[0][0], dict[0][1], alpha = 0.3, c = 'darkblue')
-ax[1, 0].scatter(dict[1][0], dict[1][1], alpha = 0.3, c = 'darkblue')
-ax[2, 0].scatter(dict[2][0], dict[2][1], alpha = 0.3, c = 'darkblue')
-ax[3, 0].scatter(dict[3][0], dict[3][1], alpha = 0.3, c = 'darkblue')
-ax[3, 0].set_xticks([0, 10000000, 20000000, 30000000, 40000000, 50000000, 60000000, 70000000])
-ax[3, 0].set_xticklabels(['0', '10', '20', '30', '40', '50', '60', '70'])
-ax[3, 0].xaxis.set_minor_locator(ticker.MultipleLocator(5000000))
-ax[0, 1].scatter(dict[4][0], dict[4][1], alpha = 0.3, c = 'darkblue')
-ax[1, 1].scatter(dict[5][0], dict[5][1], alpha = 0.3, c = 'darkblue')
-ax[2, 1].scatter(dict[6][0], dict[6][1], alpha = 0.3, c = 'darkblue')
-ax[3, 1].scatter(dict[7][0], dict[7][1], alpha = 0.3, c = 'darkblue')
-ax[3, 1].set_xticks([0, 10000000, 20000000, 30000000, 40000000, 50000000])
-ax[3, 1].set_xticklabels(['0', '10', '20', '30', '40', '50'])
-ax[3, 1].xaxis.set_minor_locator(ticker.MultipleLocator(5000000))
-ax[0, 0].yaxis.set_major_locator(ticker.MultipleLocator(50))
-ax[1, 0].yaxis.set_major_locator(ticker.MultipleLocator(50))
-ax[2, 0].set_yticks([0, 50, 100, 150], ['0', '', '100', ''])
-ax[3, 0].yaxis.set_major_locator(ticker.MultipleLocator(50))
-ax[0, 0].yaxis.set_minor_locator(ticker.MultipleLocator(25))
-ax[1, 0].yaxis.set_minor_locator(ticker.MultipleLocator(25))
-ax[2, 0].yaxis.set_minor_locator(ticker.MultipleLocator(25))
-ax[3, 0].yaxis.set_minor_locator(ticker.MultipleLocator(25))
+for i in range(0, 4):
+    ax[i, 0].scatter(dict[i][0], dict[i][1], alpha = 0.3, c = graph_color)
+    ax[i, 0].yaxis.set_major_locator(ticker.MultipleLocator(50))
+    ax[i, 0].yaxis.set_minor_locator(ticker.MultipleLocator(25))
+    ax[i, 0].set_title(titles[i], fontsize = 10)
+for i in range(4, 8):
+    ax[i-4, 1].scatter(dict[i][0], dict[i][1], alpha = 0.3, c = graph_color)
+    ax[i-4, 1].yaxis.set_major_locator(ticker.MultipleLocator(50))
+    ax[i-4, 1].yaxis.set_minor_locator(ticker.MultipleLocator(25))
+    ax[i-4, 1].set_title(titles[i], fontsize = 10)
+for i in range(0, 2):
+    ax[3, i].set_xticks([0, 10000000, 20000000, 30000000, 40000000, 50000000, 60000000, 70000000])
+    ax[3, i].set_xticklabels(['0', '10', '20', '30', '40', '50', '60', '70'])
+    ax[3, i].xaxis.set_minor_locator(ticker.MultipleLocator(5000000))
 fig.supxlabel('Distance (Mbp)')
 fig.supylabel('Distance (cM)')
 fig.suptitle('Linkage Map by Chromosome')
-ax[0, 0].set_title('Chromosome 1', fontsize = 10)
-ax[1, 0].set_title('Chromosome 2', fontsize = 10)
-ax[2, 0].set_title('Chromosome 3', fontsize = 10)
-ax[3, 0].set_title('Chromosome 4', fontsize = 10)
-ax[0, 1].set_title('Chromosome 5', fontsize = 10)
-ax[1, 1].set_title('Chromosome 6', fontsize = 10)
-ax[2, 1].set_title('Chromosome 7', fontsize = 10)
-ax[3, 1].set_title('Chromosome 8', fontsize = 10)
 plt.show()
+
 
