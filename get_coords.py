2de38baf223d3610d38e6966c252bd9c01393659
n-schlenk <99570595+n-schlenk@users.noreply.github.com>
2023-02-20 16:20:27 -0800

    Update get_coords.py

--- get_coords.py
+++ get_coords.py
@@ -2,31 +2,38 @@
 # created by N. Schlenk
 # generate final map coordinates for each scaffold
 
-map_filepath = 'sc'             # prefix of each map file (to be followed by <number>.txt)
-out_filepath = 'coords_sc'      # prefix of each coord file (to be followed by <number>.txt)
-vcf_filepath = <filepath for VCF with SNP locations>
-first_scaffold = 0
-last_scaffold = 7
 
+
+#! /usr/bin/env
+# created by N. Schlenk
+# generate final map coordinates
 import re
 import os
+
+first_scaffold = 0
+last_scaffold = 7
+scaffold_index = 12
+map_prefix = 'ordered_scaffold_'
+coords_prefix = 'coords/coords'
+invcf=open('big_vcfs/minminor1_pc.vcf', 'r')
+dict = {}
+for line in invcf:
+    if line[0] != '#' and line[0:2] != 'KA' and line[0:3] != 'POS':
+        pos = line.split('\t')[1]
+        chr = line[scaffold_index]
+        dict.setdefault(chr, [])
+        dict[chr].append(pos)
+
 for i in range(first_scaffold, last_scaffold + 1):
-    inmap=open(map_filepath + str(i) + '.txt', 'r')
-    invcf=open(vcf_filepath, 'r')
-    outfile=open(out_filepath+ str(i) + '.txt', 'w')
-    bp = {}
-    cm = {}
     n = 0
-    for line in invcf:
-        if line[0] != '#' and line[0:2] != 'KA' and line[0:3] != 'POS':
-            pos = line.split('\t')[1]
-            n += 1
-            bp[str(n)] = pos
+    inmap=open(map_prefix + str(i) + '.txt', 'r')
+    outfile=open(coords_prefix + str(i) + '.txt', 'w')
     for line in inmap:
-        if line.split()[0] in bp.keys():
-            x = bp[line.split()[0]]
-            y = (line.split()[1])
-            outline = str(x) + '\t' + str(y)
+        if line[0] != '#' and len(line.split()) > 1:
+            print(line)
+            outline = str(dict[str(i)][n]) + '\t' + str(line.split()[1])
             outfile.write(outline + '\n')
+            n += 1
+
 
 
