1928da3ac83dadd8ae4282a59dc16085602b20ec
n-schlenk <99570595+n-schlenk@users.noreply.github.com>
2023-04-28 15:43:31 -0700

    Delete BCF_callvariants.sh

--- BCF_callvariants.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# call variants on BAM files (must be in 'bam_files' directory), output one VCF
-# created by N. Schlenk
-
-module load bcftools
-ls ./bam_files > bam_list.txt
-bcftools mpileup -Ou -I -a FORMAT/AD --max-depth=100 -f PGA_assembly_trimmed.fasta -b bam_list.txt | bcftools call -vmO v > called.vcf
